57a681a23f8a9d013aaa14588f47ac9ab75c458f
Sajjad Shojaei Moghaddam <j10110000110@gmail.com>
2022-02-03 12:22:45 +0330

    binance api test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BinanceModule } from './binance/binance.module';
+import { TestnetService } from "./testnet/services/testnet.service";
+import { ControllersController } from './testnet/controllers/controllers.controller';
+import { TestnetModule } from './testnet/testnet.module';
+
 
 @Module({
-  imports: [BinanceModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BinanceModule, TestnetModule],
+  controllers: [AppController, ControllersController],
+  providers: [AppService, TestnetService],
 })
 export class AppModule {}
